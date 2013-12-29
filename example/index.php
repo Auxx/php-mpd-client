7ec4d9ee9004c31fb020009a321fc7c72b345381
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-12-29 19:46:06 +0200

    Fixed exceptions

--- example/index.php
+++ example/index.php
@@ -5,7 +5,14 @@ include '../src/php_mpd_client.php';
 $client = new PhpMpdClient();
 echo 'Is connected: '.($client->isConnected() ? 'TRUE' : 'FALSE')."\n";
 
-$command = new MpdCommand('status');
-$client->execute($command);
+$client->execute(new MpdCommand('status'));
+try {
+	$client->execute(new MpdCommand('penis', array(1)));
+}
+catch(Exception $e) {
+	print_r($e);
+}
+
+
 
 ?>
